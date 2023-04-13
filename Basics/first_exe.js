8034f63ed81d316d29cf36705e34e5d1062d90fd
vaskoninov <vasko.ninov@gmail.com>
2023-04-13 08:49:45 +0300

    new tasks

---
+++ Basics/first_exe.js
@@ -0,0 +1,103 @@
+// function converter(input) {
+//     const USD = 1.79549;
+//     let lv = input[0];
+
+//     let converted = USD * lv;
+//     console.log(converted)
+// }
+
+// converter(["22"])
+
+//////////////////
+
+// function converter(input) {
+//     let radians = Number(input[0]);
+//     let degrees = radians * 180 / Math.PI;
+
+//     console.log(degrees);
+// }
+
+// converter(["3.1416"])
+
+//////////////
+
+// function depositCalc(input) {
+//     let amount = Number(input[0]);
+//     let time = Number(input[1]);
+//     let interest = Number(input[2])/100;
+
+//     let total = amount + time * ((amount * interest) / 12);
+
+//     console.log(total)
+// }
+
+// depositCalc(["200", "3", "5.7"])
+
+///////////////
+
+// function reader(input) {
+//     let pages_per_book = Number(input[0]);
+//     let pages_per_hour = Number(input[1]);
+//     let days = Number(input[2]);
+
+//     let hours_per_day = pages_per_book / pages_per_hour / days;
+
+//     console.log(hours_per_day);
+// }
+
+// reader(["212", "20", "2"]);
+
+////////////////
+
+// function calculateSchool(input) {
+//     let pen_price = 5.8;
+//     let marker_price = 7.2;
+//     let cleaning_price = 1.2;
+
+//     let pen_packs = Number(input[0]);
+//     let marker_packs = Number(input[1]);
+//     let cleaning_litters = Number(input[2]);
+//     let discount_percentage = Number(input[3]) / 100;
+
+//     let total_amount =
+//         pen_packs * pen_price +
+//         marker_packs * marker_price +
+//         cleaning_litters * cleaning_price;
+//     let discount = total_amount * discount_percentage;
+//     let final = total_amount - discount;
+
+//     console.log(final);
+// }
+
+// calculateSchool(["2 ", "3 ", "4 ", "25 "]);
+
+//////////////
+
+// function repainting(input) {
+//     let nylon_per_meter = 1.5;
+//     let paint_per_litter = 14.5;
+//     let razr_per_litter = 5.0;
+
+//     let bags = 0.4;
+
+//     let nylon = Number(input[0]) + 2;
+//     let paint = Number(input[1]) * 1.1;
+//     let razr = Number(input[2]);
+//     let hours = Number(input[3]);
+
+//     let materials =
+//         nylon * nylon_per_meter +
+//         paint_per_litter * paint +
+//         razr * razr_per_litter +
+//         bags;
+
+//     let per_hour = materials * 0.3;
+
+//     let work_price = per_hour * hours;
+
+//     let total = materials + work_price;
+
+//     console.log(total);
+// }
+
+// repainting(["10 ", "11 ", "4 ", "8 "]);
