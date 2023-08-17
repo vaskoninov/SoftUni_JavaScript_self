4cea23d6e5951da7b42b785d483e1fda14314149
Vasil Ninov <30572741+vaskoninov@users.noreply.github.com>
2023-08-17 09:42:38 +0000

    б

---
+++ Fundamentals/intro_lec.js
@@ -0,0 +1,68 @@
+/// Multiply by 2
+
+// function mult(num) {
+//     console.log(num * 2);
+// }
+// mult(2)
+
+/// Student Info
+
+// function getInfo(name, age, grade) {
+//     console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+// }
+// getInfo('John', 15, 5.54678)
+
+/// Excellent Grade
+
+// function getGrade(num) {
+//     if (num >= 5.50) {
+//         console.log("Excellent");
+//     } else {
+//         console.log("Not excellent");
+//     }
+// }
+
+/// Foreign Language
+
+// function getLanguage(country) {
+//     switch (country) {
+//         case "USA":
+//         case "England":
+//             console.log("English");
+//             break;
+//         case "Spain":
+//         case "Argentina":
+//         case "Mexico":
+//             console.log("Spanish");
+//             break;
+//         default:
+//             console.log("unknown");
+//             break;
+//     }
+// }
+
+//// Month Printer
+
+// function getMonth(month_number) {
+//     let months = ["January",
+//         "February",
+//         "March",
+//         "April",
+//         "May",
+//         "June",
+//         "July",
+//         "August",
+//         "September",
+//         "October",
+//         "November",
+//         "December",
+//     ];
+//     if (month_number >= 1 && month_number <= 12) {
+//         console.log(months[month_number - 1]);
+//     } else {
+//         console.log("Error!")
+//     }
+// }
+// getMonth(13)
+
+/// Theatre Promotion
