84495792aab5b75727957ab9237caab7d9317b0b
Vasil Ninov <30572741+vaskoninov@users.noreply.github.com>
2023-08-17 04:46:04 +0000

    m

--- Basics/nested_cycles_lecture.js
+++ Basics/nested_cycles_lecture.js
@@ -39,33 +39,93 @@
 
 /// Sum of Numbers
 
-function sumNumbers(input) {
-    let a = Number(input[0]);
-    let b = Number(input[1]);
-    let target = Number(input[2]);
+// function sumNumbers(input) {
+//     let a = Number(input[0]);
+//     let b = Number(input[1]);
+//     let target = Number(input[2]);
 
-    let combs = 0;
-    let found = false;
-    let expression = null;
+//     let combs = 0;
+//     let found = false;
+//     let expression = null;
 
-    for (let i = a; i <= b; i++) {
-        for (let j = a; j <= b; j++) {
-            combs++;
-            if ((i + j) === target) {
-                found = true;
-                expression = `(${i} + ${j} = ${target})`;
-                break;
-            }
-        }
-        if (found) {
-            break;
-        }
-    }
-    if (found) {
-        console.log(`Combination N:${combs} ${expression}`);
-    } else {
-        console.log(`${combs} combinations - neither equals ${target}`)
-    }
-}
+//     for (let i = a; i <= b; i++) {
+//         for (let j = a; j <= b; j++) {
+//             combs++;
+//             if ((i + j) === target) {
+//                 found = true;
+//                 expression = `(${i} + ${j} = ${target})`;
+//                 break;
+//             }
+//         }
+//         if (found) {
+//             break;
+//         }
+//     }
+//     if (found) {
+//         console.log(`Combination N:${combs} ${expression}`);
+//     } else {
+//         console.log(`${combs} combinations - neither equals ${target}`)
+//     }
+// }
 
-sumNumbers(["23", "24", "20"])
+// sumNumbers(["23", "24", "20"])
+
+/// Travelling
+
+// function toTravel(input) {
+//     let i = 0;
+//     while (true) {
+//         let destination = input[i];
+//         if (destination === "End") break;
+//         ++i;
+//         let cost = Number(input[i]);
+//         ++i;
+//         let gathered = 0;
+
+//         while (gathered < cost) {
+//             gathered += Number(input[i]);
+//             i++;
+//         }
+//         console.log(`Going to ${destination}!`);
+//     }
+// }
+
+// toTravel(["Greece",
+//     "1000",
+//     "200",
+//     "200",
+//     "300",
+//     "100",
+//     "150",
+//     "240",
+//     "Spain",
+//     "1200",
+//     "300",
+//     "500",
+//     "193",
+//     "423",
+//     "End"]);
+
+///// Building
+
+// function showRoomNumbers(input) {
+//     let floors = Number(input[0]);
+//     let rooms = Number(input[1]);
+
+//     for (let i = floors; i > 0; i--) {
+//         let numbers = "";
+//         for (let j = 0; j < rooms; j++) {
+
+//             if (i === floors) {
+//                 numbers += `L${i}${j} `;
+//             } else if (i % 2 == 0) {
+//                 numbers += `O${i}${j} `;
+//             } else if (i % 2 != 0) {
+//                 numbers += `A${i}${j} `;
+//             }
+//         }
+//         console.log(numbers);
+//     }
+// }
+
+// showRoomNumbers(["1", "4"]);
