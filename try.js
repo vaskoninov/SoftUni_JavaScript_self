a6f1bdfe46b5254860e72212c7855751c83bf15b
vaskoninov <vasko.ninov@gmail.com>
2023-08-21 21:30:35 +0300

    m

--- try.js
+++ try.js
@@ -1,10 +1,17 @@
-function calculateCalc(numOne, numTwo, operator) {
-    let calc = {
-        "add": (a, b) => a + b,
-        "divide": (a, b) => b != 0 ? a / b : 0,
-        "multiply": (a, b) => a * b,
-        "subtract": (a, b) => a - b,
-    };
-    console.log(calc[operator](numOne, numTwo));
+// function calculateCalc(numOne, numTwo, operator) {
+//     let calc = {
+//         "add": (a, b) => a + b,
+//         "divide": (a, b) => b != 0 ? a / b : 0,
+//         "multiply": (a, b) => a * b,
+//         "subtract": (a, b) => a - b,
+//     };
+//     console.log(calc[operator](numOne, numTwo));
+// }
+// calculateCalc(2, 0, "divide");
+
+let ass_arr = {
+    "key": (a) => a,
 }
-calculateCalc(2, 0, "divide");
+
+let x = "key";
+console.log(ass_arr[x](8));
