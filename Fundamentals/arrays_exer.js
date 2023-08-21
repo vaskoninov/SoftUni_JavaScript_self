ed53b8902dce129d03621ff899e9a1edade37362
Vasil Ninov <30572741+vaskoninov@users.noreply.github.com>
2023-08-21 11:20:10 +0000

    m

--- Fundamentals/arrays_exer.js
+++ Fundamentals/arrays_exer.js
@@ -83,26 +83,196 @@
 
 ///// Equal Sums
 
-function checkEqualSums(array) {
-    if (array.length === 1) {
-        console.log(0);
-    } else if (array.length > 1) {
-        let equality = false;
-        for (let i = 1; i < array.length; i++) {
-            let first = array.slice(0, i);
-            let second = array.slice(i + 1);
-            let sumFirst = first.reduce((a, b) => a + b, 0);
-            let sumSec = second.reduce((a, b) => a + b, 0);
-            if (sumFirst === sumSec) {
-                equality = true;
-                break;
-            }
-        }
-        if (equality) {
-            console.log(i);
-        } else {
-            console.log("no");
+// function checkEqualSums(array) {
+//     if (array.length === 1) {
+//         console.log(0);
+//     } else if (array.length > 1) {
+//         let equality = false;
+//         let index = null;
+//         for (let i = 1; i < array.length; i++) {
+//             let first = array.slice(0, i);
+//             let second = array.slice(i + 1);
+//             let sumFirst = first.reduce((a, b) => a + b, 0);
+//             let sumSec = second.reduce((a, b) => a + b, 0);
+//             if (sumFirst === sumSec) {
+//                 equality = true;
+//                 index = i;
+//                 break;
+//             }
+//         }
+//         if (equality) {
+//             console.log(index);
+//         } else {
+//             console.log("no");
+//         }
+//     }
+// }
+// checkEqualSums([1, 2]);
+
+////// Max Sequence of Same Elements
+
+// function getMaxSequence(seq) {
+
+//     let max_length = 0;
+//     let start_index = 0;
+
+//     for (let i = 0; i < seq.length; i++) {
+//         let temp_length = 1;
+//         for (let j = i + 1; j < seq.length; j++) {
+//             if (seq[i] === seq[j]) {
+//                 temp_length++;
+//             } else {
+//                 break;
+//             }
+//         }
+//         if (temp_length > max_length){ 
+//             max_length = temp_length;
+//             start_index = i;
+//         }
+//     }
+
+//     console.log(seq.slice(start_index, start_index + max_length).join(" "));
+
+// }
+
+// getMaxSequence([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
+
+///// Magic Sum
+
+// function solve(seq, num) {
+//     for (let i = 0; i < seq.length; i++) {
+//         for (let j = i + 1; j < seq.length; j++) {
+//             if ((seq[i] + seq[j]) === num) {
+//                 console.log(seq[i], seq[j]);
+//             }
+//         }
+//     }
+// }
+
+// solve([1, 7, 6, 2, 19, 23], 8);
+// solve([14, 20, 60, 13, 7, 19, 8],
+//     27);
+
+
+// ///////// Dungeonest Dark
+
+// function adventures(instructions) {
+//     let health = 100;
+//     let coins = 0;
+//     let dead = false;
+//     let rooms = String(instructions).split("|");
+
+//     for (let i = 0; i < rooms.length; i++) {
+//         let room = rooms[i];
+//         room = room.split(" ");
+//         let monster = room[0];
+//         let value = Number(room[1]);
+
+//         if (monster === "potion") {
+//             if (health + value > 100) {
+//                 hp = 100 - health;
+//                 health = 100;
+//             } else {
+//                 health += value;
+//                 hp = value;
+//             }
+//             console.log(`You healed for ${hp} hp.`);
+//             console.log(`Current health: ${health} hp.`);
+//         } else if (monster === "chest") {
+//             coins += value;
+//             console.log(`You found ${value} coins.`);
+//         } else {
+//             health -= value;
+//             if (health > 0) {
+//                 console.log(`You slayed ${monster}.`);
+//             } else {
+//                 console.log(`You died! Killed by ${monster}.`);
+//                 console.log(`Best room: ${i + 1}`);
+//                 dead = true;
+//                 break;
+//             }
+//         }
+//     }
+//     if (!(dead)) {
+//         console.log("You've made it!");
+//         console.log(`Coins: ${coins}`);
+//         console.log(`Health: ${health}`);
+//     }
+
+// }
+// adventures(["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"]);
+// console.log("___________________");
+// adventures(["cat 10|potion 30|orc 10|chest 10|snake 25|chest 110"]);
+
+///////// Lady Bugs
+
+function ladyBugs(seq) {
+    let size = seq[0];
+    let initial_locations = seq[1].split(" ").map(Number);
+
+    let field = [];
+    for (let i = 0; i < size; i++) {
+        field.push(0);
+    }
+
+
+    for (let el of initial_locations) {
+        if (el >= 0 && el < size) {
+            field[el] = "lady";
         }
     }
+
+
+    for (let i = 2; i < seq.length; i++) {
+        let instructions = seq[i].split(" ");
+        let start = Number(instructions[0]);
+        if (start < 0 || start >= size || (field[start] !== "lady")) continue;
+        let end = Number(instructions[instructions.length - 1]);
+        let direction = instructions[1];
+        if (end < 0) {
+            end = Math.abs(end);
+            direction = command === "right" ? "left" : "right";
+        }
+        field[start] = 0;
+
+
+        if (direction === "right") {
+            let newPosition = start + end;
+            if (field[newPosition] === "lady") {
+                newPosition = newPosition + end;
+            }
+            if (newPosition <= field.length) {
+                field[newPosition] = "lady";
+            }
+        } else {
+            let newPosition = start - end;
+            if (field[newPosition] === "lady") {
+                newPosition = newPosition - end;
+            }
+            if (newPosition >= 0) {
+                field[newPosition] = "lady";
+            }
+        }
+        
+    }
+
+    for (let i = 0; i < field.length; i++) {
+        if (field[i] === "lady") field[i] = 1;
+    }
+    console.log(field.join(" "));
 }
-checkEqualSums([1, 2, 3, 3]);
+
+// ladyBugs([3, '0 1',
+//     '0 right 1',
+//     '2 right 1']);
+ladyBugs([3, '0 1 2',
+    '0 right 1',
+    '1 right 1',
+    '2 right 1']
+);
+
+// ladyBugs([3, '0 1 2',
+//     '0 right 1',
+//     '1 right 1',
+//     '2 right 1']
+// );
